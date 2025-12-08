c519bb4c7f1b43d84c2baf754db9aaae44fe9d2a
one-centavo <gustavoadolfocabrera15@gmail.com>
2025-12-08 15:19:54 -0500

    I've added the class main database setup and create the methods connection and query

---
+++ app/main.php
@@ -0,0 +1,36 @@
+<?php
+
+    namespace App;
+    use PDO;
+    use PDOException;
+    if(file_exists(__DIR__.'/../config/server.php')){
+        require_once __DIR__.'/../config/server.php';
+    }
+
+    class main{
+        private $server = DB_SERVER;
+        private $username = DB_USER;
+        private $pass = DB_PASS;
+        private $db = DB_NAME;
+        protected function connection(){
+            $config = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false,
+            ];
+
+            $dns = "mysql:host=$this->server;dbname=$this->db;charset=utf8mb4";
+            try{
+                $pdo = new PDO($dns, $this->username, $this->pass, $config);
+                return $pdo;
+            }catch(PDOException $e){
+                die("Connection failed: " . $e->getMessage() . " in the line " . $e->getLine() . " of the file " . $e->getFile());
+            }
+        }
+
+        protected function query($sql, $data = []){
+            $stmt = $this->connection()->prepare($sql);
+            $stmt->execute($data);
+            return $stmt;
+        }
+    }
--- config/server.php
+++ config/server.php
@@ -1,6 +1,6 @@
 <?php
 
     const DB_SERVER = "localhost";
-    const DB_NAME = "STUDENTS-DIRECTORY";
+    const DB_NAME = "students_directory";
     const DB_USER = "root";
     const DB_PASS = "";
